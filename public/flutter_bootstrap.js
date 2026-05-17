6318563a648abbf81cbadc4602c223d4074c177a
yitu-dev <kingayenew9@gmail.com>
2026-05-17 13:47:29 +0300

    feat: persist dashboard active screen state across browser refreshes

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1923562081" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "76667279" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
